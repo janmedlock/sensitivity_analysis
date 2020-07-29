9e779245e4ac8e0a7feee3ea606192a66e58829b
Jan Medlock <jan.medlock@oregonstate.edu>
2020-07-29 13:43:04 -0700

    Add a function `PRCC_samples(X, y)` parallel to other methods.

--- sensitivity_analysis/PRCC_.py
+++ sensitivity_analysis/PRCC_.py
@@ -5,7 +5,13 @@ from . import stats
 from . import _util
 
 
+def PRCC_samples(X, y):
+    '''The partial rank correlation coefficient.'''
+    return stats.PRCC(X, y)
+
+
 def PRCC(model, parameters, n_samples, seed=None):
+    '''The partial rank correlation coefficient.'''
     X = sampling.samples_Latin_hypercube(parameters, n_samples, seed=seed)
     y = _util.model_eval(model, X)
-    return stats.PRCC(X, y)
+    return PRCC_samples(X, y)
